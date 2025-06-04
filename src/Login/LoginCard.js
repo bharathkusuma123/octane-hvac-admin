d7510e1ebb96b8758dc26f94ec5bb81ae8609eec
Tharun <tharunkumarreddy1212@gmail.com>
2025-06-04 12:07:02 +0530

    Center Card Admin

--- src/Login/LoginCard.js
+++ src/Login/LoginCard.js
@@ -11,6 +11,7 @@ import {
 } from "react-bootstrap";
 import { EyeFill, EyeSlashFill } from "react-bootstrap-icons";
 import logo from "../Logos/hvac-logo-new.jpg";
+import "./Login.css"
 
 const LoginCard = ({
   title,
@@ -24,62 +25,64 @@ const LoginCard = ({
   error,
 }) => {
   return (
-    <Container className="mt-5">
-      <Row className="justify-content-center">
-        <Col md={6} lg={4}>
-          <Card className="shadow">
-            <Card.Body>
-              <div className="text-center mb-4">
-                <img
-                  src={logo}
-                  alt="Company Logo"
-                  style={{ width: "150px", height: "80px" }}
-                />
-                <h3 className="mt-3">{title}</h3>
-              </div>
-
-              {error && <Alert variant="danger">{error}</Alert>}
-
-              <Form onSubmit={handleSubmit}>
-               <Form.Group className="mb-3">
-                  <Form.Label>Username</Form.Label>
-                  <Form.Control
-                    type="text"
-                    placeholder="Enter username"
-                    value={username}
-                    onChange={(e) => setUsername(e.target.value)}
-                    required
+    <div className="d-flex align-items-center justify-content-center min-vh-100">
+      <Container className="mt-5">
+        <Row className="justify-content-center">
+          <Col md={6} lg={4}>
+            <Card className="shadow">
+              <Card.Body>
+                <div className="text-center mb-4">
+                  <img
+                    src={logo}
+                    alt="Company Logo"
+                    style={{ width: "150px", height: "80px" }}
                   />
-                </Form.Group>
+                  <h3 className="mt-3">{title}</h3>
+                </div>
 
+                {error && <Alert variant="danger">{error}</Alert>}
+
+                <Form onSubmit={handleSubmit}>
                 <Form.Group className="mb-3">
-                  <Form.Label>Password</Form.Label>
-                  <InputGroup>
+                    <Form.Label>Username</Form.Label>
                     <Form.Control
-                      type={showPassword ? "text" : "password"}
-                      placeholder="Password"
-                      value={password}
-                      onChange={(e) => setPassword(e.target.value)}
+                      type="text"
+                      placeholder="Enter username"
+                      value={username}
+                      onChange={(e) => setUsername(e.target.value)}
                       required
                     />
-                    <Button
-                      variant="outline-secondary"
-                      onClick={() => setShowPassword(!showPassword)}
-                    >
-                      {showPassword ? <EyeSlashFill /> : <EyeFill />}
-                    </Button>
-                  </InputGroup>
-                </Form.Group>
+                  </Form.Group>
 
-                <Button variant="primary" type="submit" className="w-100">
-                  Login
-                </Button>
-              </Form>
-            </Card.Body>
-          </Card>
-        </Col>
-      </Row>
-    </Container>
+                  <Form.Group className="mb-3">
+                    <Form.Label>Password</Form.Label>
+                    <InputGroup>
+                      <Form.Control
+                        type={showPassword ? "text" : "password"}
+                        placeholder="Password"
+                        value={password}
+                        onChange={(e) => setPassword(e.target.value)}
+                        required
+                      />
+                      <Button
+                        variant="outline-secondary"
+                        onClick={() => setShowPassword(!showPassword)}
+                      >
+                        {showPassword ? <EyeSlashFill /> : <EyeFill />}
+                      </Button>
+                    </InputGroup>
+                  </Form.Group>
+
+                  <Button variant="primary" type="submit" className="loginButton shadow">
+                    Login
+                  </Button>
+                </Form>
+              </Card.Body>
+            </Card>
+          </Col>
+        </Row>
+      </Container>
+    </div>
   );
 };
 
